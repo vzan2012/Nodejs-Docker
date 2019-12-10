79716fb58efa2ea286770e80629ac7345b6f3940
Deepak Guptha <vzan2012@gmail.com>
2019-12-10 12:36:39 +0530

    feat(user): Dockerfile updated

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 
 const port = process.env.PORT || 3000;
 
-app.get((req, res) => res.send("Hi there"));
+app.get("/", (req, res) => res.send("Hi there"));
 
 // Listening to the PORT
 app.listen(port, () => console.log(`Listening to the port ${port}`));
